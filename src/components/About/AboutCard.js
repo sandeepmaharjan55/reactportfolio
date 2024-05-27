44c2b2230552890d3b95babbfd5960efcb08f78c
Sandeep Maharjan <sandeepmaharjan55@gmail.com>
2024-05-27 12:26:30 -0500

    about changes

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -22,7 +22,7 @@ function AboutCard() {
               My contributions led to enhanced system performance, scalability, and improved database management
                with a focus on MongoDB.
                <br />During my tenure as a Software Engineer at Bigfoot Infotech Pvt. Ltd., 
-               I collaborated closely with other software engineers to troubleshoot NodeJS app issues,
+               I collaborated closely with experienced software engineers to troubleshoot NodeJS app issues,
                documented code and processes for team knowledge sharing, implemented middleware components, and collaborated with various stakeholders. I enhanced product performance, added functionality, corrected coding issues, and updated interfaces. I also stayed updated with the latest backend development trends and technologies.
                 <br />As the Co-founder and Software Engineer at Walkman by Green Coins, 
                   I spearheaded the development of the Android mobile application's back-end using Node Js. 
