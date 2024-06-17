a151a81fcc6bb6e32a480b25856bb126556b803e
Sandeep Maharjan <sandeepmaharjan55@gmail.com>
2024-06-17 10:49:01 -0500

    github: project file link changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -136,7 +136,7 @@ function Projects() {
               title="Reverse Engineering Geogebra XML"
               description="A Java project to add points, lines, and polygons on a 2D plane (with Menu Bar, Drawing Canvas, Control Panel).The main window of the application is built with JFrame object from the Java Swing API. Features: Draw 2D elements, read and write files to and from GeoGebra.org.
               Skills: Java · JFrame"
-              ghLink="https://github.com/sandeepmaharjan55/xmlgeogebra"
+              ghLink="https://github.com/sandeepmaharjan55/xmlgeogebra/tree/V3"
               demoLink="https://www.loom.com/share/e632a35d4cae40109bbc05130599ccfc?sid=1631019d-17bf-44f1-b14e-aa46a746cd43"
             />
           </Col>
